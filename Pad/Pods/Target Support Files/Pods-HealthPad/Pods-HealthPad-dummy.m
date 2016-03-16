f5fbd9879810827ee23b6ffd502061cebd0cb002
Finn Gaida <f@fga.pw>
2016-03-16 14:02:41 +0100

    add iPad Project

---
+++ Pad/Pods/Target Support Files/Pods-HealthPad/Pods-HealthPad-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HealthPad : NSObject
+@end
+@implementation PodsDummy_Pods_HealthPad
+@end
---
+++ Phone/Pods/Target Support Files/Pods-HealthPad Companion/Pods-HealthPad Companion-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_HealthPad_CompanionVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_HealthPad_CompanionVersionString[];
+
