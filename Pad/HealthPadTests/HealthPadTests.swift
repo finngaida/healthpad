36fbc220f60886289fe9c65b333c1280b57df5e2
Finn Gaida <f@fga.pw>
2016-03-25 21:29:53 +0000

    add Quick + Nible for tests

--- Pad/HealthPadTests/HealthPadTests.swift
+++ Pad/HealthPadTests/HealthPadTests.swift
@@ -7,6 +7,8 @@
 //
 
 import XCTest
+import Quick
+import Nimble
 import CloudKit
 @testable import HealthPad
 
---
+++ Pad/Pods/Target Support Files/Pods-HealthPadTests/Pods-HealthPadTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_HealthPadTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_HealthPadTestsVersionString[];
+
